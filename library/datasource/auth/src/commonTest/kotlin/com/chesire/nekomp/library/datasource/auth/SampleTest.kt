b91dac3160d9883eec4169af4478e5604efce2a8
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-02-07 09:36:08 +0000

    Add sample test so ci is happier

---
+++ library/datasource/auth/src/commonTest/kotlin/com/chesire/nekomp/library/datasource/auth/SampleTest.kt
@@ -0,0 +1,14 @@
+package com.chesire.nekomp.library.datasource.auth
+
+import com.chesire.nekomp.library.datasource.auth.model.LoginRequestDto
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class SampleTest {
+
+    @Test
+    fun `Sample test remove later`() {
+        val a = LoginRequestDto("Username", "Password", "GrantType")
+        assertTrue { a.username == "Username" }
+    }
+}
