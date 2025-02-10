3688bc3d8c98050cc5caddc8173cd1266fd8d411
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-02-10 15:13:38 +0000

    Detekt fix

--- library/datasource/library/src/commonMain/kotlin/com/chesire/nekomp/library/datasource/library/LibraryRepository.kt
+++ library/datasource/library/src/commonMain/kotlin/com/chesire/nekomp/library/datasource/library/LibraryRepository.kt
@@ -3,13 +3,16 @@ package com.chesire.nekomp.library.datasource.library
 import com.chesire.nekomp.library.datasource.library.remote.LibraryApi
 import com.chesire.nekomp.library.datasource.user.UserRepository
 
+private const val LIMIT = 20
+
 class LibraryRepository(
     private val libraryApi: LibraryApi,
     private val userRepository: UserRepository
 ) {
 
     suspend fun retrieve(): Result<String> {
-        return libraryApi.retrieveAnime(userRepository.user.id, 0, 20)
+        // For now retrieve 20 anime, it should cycle through getting all of them using pagination
+        return libraryApi.retrieveAnime(userRepository.user.id, 0, LIMIT)
             .map {
                 "Test"
             }
