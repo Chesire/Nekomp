efc0067f418e68d7e84ff9485d91da46f3c9825c
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-02-14 10:44:36 +0000

    Adapt UI as screen size changes

--- feature/library/src/commonMain/kotlin/com/chesire/nekomp/feature/library/ui/LibraryScreen.kt
+++ feature/library/src/commonMain/kotlin/com/chesire/nekomp/feature/library/ui/LibraryScreen.kt
@@ -1,5 +1,10 @@
+@file:OptIn(ExperimentalMaterial3AdaptiveApi::class, ExperimentalSharedTransitionApi::class)
+
 package com.chesire.nekomp.feature.library.ui
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,11 +15,21 @@ import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.layout.AnimatedPane
+import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffold
+import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
+import androidx.compose.material3.adaptive.layout.PaneAdaptedValue
+import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -22,16 +37,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
+import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
-fun LibraryScreen(
-    viewModel: LibraryViewModel = koinViewModel()
-) {
+fun LibraryScreen(viewModel: LibraryViewModel = koinViewModel()) {
     val state by viewModel.uiState.collectAsState()
 
     Render(
@@ -45,8 +60,12 @@ private fun Render(
     state: UIState,
     execute: (ViewAction) -> Unit
 ) {
-    val size = calculate
     val snackbarHostState = remember { SnackbarHostState() }
+    val navigator = rememberListDetailPaneScaffoldNavigator<Entry>()
+    val isListAndDetailVisible =
+        navigator.scaffoldValue[ListDetailPaneScaffoldRole.Detail] == PaneAdaptedValue.Expanded &&
+            navigator.scaffoldValue[ListDetailPaneScaffoldRole.List] == PaneAdaptedValue.Expanded
+
     LaunchedEffect(state.viewEvent) {
         when (state.viewEvent) {
             null -> Unit
@@ -59,35 +78,106 @@ private fun Render(
         modifier = Modifier.fillMaxSize(),
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
     ) { paddingValues ->
-        LazyColumn(
+        SharedTransitionLayout(modifier = Modifier.padding(paddingValues)) {
+            AnimatedContent(
+                targetState = isListAndDetailVisible,
+                label = "listDetailAnimatedContent"
+            ) {
+                ListDetailPaneScaffold(
+                    directive = navigator.scaffoldDirective,
+                    value = navigator.scaffoldValue,
+                    listPane = {
+                        AnimatedPane {
+                            ListContent(
+                                entries = state.entries,
+                                onEntryClick = { entry ->
+                                    navigator.navigateTo(ListDetailPaneScaffoldRole.Detail, entry)
+                                }
+                            )
+                        }
+                    },
+                    detailPane = {
+                        AnimatedPane {
+                            DetailContent(
+                                entry = navigator.currentDestination?.content,
+                                showBack = navigator.scaffoldValue.primary == PaneAdaptedValue.Expanded,
+                                goBack = { navigator.navigateBack() }
+                            )
+                        }
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun ListContent(
+    entries: ImmutableList<Entry>,
+    onEntryClick: (Entry) -> Unit
+) {
+    LazyColumn(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(
+            items = entries,
+            key = { it.title }
+        ) {
+            ListItem(it, onEntryClick)
+        }
+    }
+}
+
+@Composable
+private fun ListItem(entry: Entry, onEntryClick: (Entry) -> Unit) {
+    ElevatedCard(
+        onClick = { onEntryClick(entry) },
+        modifier = Modifier
+            .widthIn(max = 360.dp)
+            .padding(8.dp)
+    ) {
+        Row {
+            AsyncImage(
+                model = entry.image,
+                contentDescription = null,
+                modifier = Modifier.requiredHeight(200.dp)
+            )
+            Column(modifier = Modifier.padding(8.dp)) {
+                Text(
+                    text = entry.title,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun DetailContent(
+    entry: Entry?,
+    showBack: Boolean,
+    goBack: () -> Unit
+) {
+    if (entry != null) {
+        Column(
             modifier = Modifier.padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            items(
-                items = state.entries,
-                key = { it.title }
-            ) {
-                ElevatedCard(
-                    modifier = Modifier
-                        .widthIn(max = 360.dp)
-                        .padding(8.dp)
+            if (showBack) {
+                IconButton(
+                    onClick = goBack,
+                    modifier = Modifier.align(Alignment.Start)
                 ) {
-                    Row {
-                        AsyncImage(
-                            model = it.image,
-                            contentDescription = null,
-                            modifier = Modifier.requiredHeight(200.dp)
-                        )
-                        Column(modifier = Modifier.padding(8.dp)) {
-                            Text(
-                                text = it.title,
-                                modifier = Modifier.fillMaxWidth()
-                            )
-                        }
-                    }
+                    Icon(
+                        painter = rememberVectorPainter(Icons.AutoMirrored.Filled.ArrowBack),
+                        contentDescription = "Go back"
+                    )
                 }
             }
+            Text(text = entry.title)
         }
     }
 }
