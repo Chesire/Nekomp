81a5c7e25914886d5959bc33a1bdc15b9288bbf1
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-02-17 14:31:37 +0000

    Pull down all anime and manga at once

--- library/datasource/library/src/commonMain/kotlin/com/chesire/nekomp/library/datasource/library/LibraryRepository.kt
+++ library/datasource/library/src/commonMain/kotlin/com/chesire/nekomp/library/datasource/library/LibraryRepository.kt
@@ -1,43 +1,88 @@
 package com.chesire.nekomp.library.datasource.library
 
+import co.touchlab.kermit.Logger
 import com.chesire.nekomp.library.datasource.library.local.LibraryStorage
 import com.chesire.nekomp.library.datasource.library.remote.LibraryApi
 import com.chesire.nekomp.library.datasource.library.remote.model.DataDto
 import com.chesire.nekomp.library.datasource.library.remote.model.IncludedDto
 import com.chesire.nekomp.library.datasource.library.remote.model.RetrieveResponseDto
 import com.chesire.nekomp.library.datasource.user.UserRepository
+import com.github.michaelbull.result.Err
+import com.github.michaelbull.result.Ok
+import com.github.michaelbull.result.Result
+import com.github.michaelbull.result.anyOk
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
+import kotlin.Result as KResult
 
 private const val LIMIT = 20
 
+// TODO: Add a remote data source that converts the dtos appropriately?
+// TODO: Handle errors and maybe retrying?
 class LibraryRepository(
     private val libraryApi: LibraryApi,
     private val libraryStorage: LibraryStorage,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository // TODO: Inject method to get the user id?
 ) {
 
     val libraryEntries: Flow<List<LibraryEntry>> = libraryStorage.libraryEntries
 
-    suspend fun retrieve(): Result<List<LibraryEntry>> {
-        // TODO: Add a remote data source that converts the dtos appropriately
-        // For now retrieve 20 anime, it should cycle through getting all of them using pagination
-        // it should also pull all the manga in the call
-        val user = userRepository.user.first()
-        if (!user.isAuthenticated) {
-            return Result.failure(Throwable()) // TODO: Add custom exception for no user
+    suspend fun retrieve(): Result<Unit, Unit> {
+        Logger.d("LibraryRepository") { "Making call to retrieve library entries" }
+        val user = userRepository.user.firstOrNull()
+        if (user?.isAuthenticated != true) {
+            Logger.e("LibraryRepository") { "No user object, cancelling retrieve" }
+            return Err(Unit) // TODO: Add custom error type
         }
-        return libraryApi.retrieveAnime(user.id, 0, LIMIT)
-            .map { dto ->
-                if (dto.included == null) {
-                    emptyList()
-                } else {
-                    buildEntries(dto)
+
+        val result = coroutineScope {
+            awaitAll(
+                async { retrieve(user.id, libraryApi::retrieveAnime) },
+                async { retrieve(user.id, libraryApi::retrieveManga) }
+            )
+        }
+
+        return if (result.anyOk()) Ok(Unit) else Err(Unit)
+    }
+
+    private suspend fun retrieve(
+        userId: Int,
+        apiCall: suspend (userId: Int, offset: Int, limit: Int) -> KResult<RetrieveResponseDto>
+    ): Result<List<LibraryEntry>, Unit> {
+        var isSuccess = false
+        val entries = mutableListOf<LibraryEntry>()
+        var offset = 0
+        var page = 0
+        var next = ""
+        do {
+            apiCall(userId, offset, LIMIT)
+                .map { dto ->
+                    next = dto.links.next
+                    page++
+                    offset = LIMIT * page
+                    if (dto.included == null) {
+                        emptyList()
+                    } else {
+                        buildEntries(dto)
+                    }
                 }
-            }
-            .onSuccess {
-                libraryStorage.updateEntries(it)
-            }
+                .onSuccess {
+                    isSuccess = true
+                    entries.addAll(it)
+                    libraryStorage.updateEntries(it)
+                }
+                .onFailure {
+                    return@retrieve Err(Unit)
+                }
+        } while (next.isNotBlank())
+
+        Logger.d("LibraryRepository") {
+            "Retrieve call $apiCall, got entries [$isSuccess], total entries ${entries.count()}"
+        }
+        return if (isSuccess) Ok(entries) else Err(Unit)
     }
 
     private fun buildEntries(body: RetrieveResponseDto): List<LibraryEntry> {
