f59c681819e33d7cdafa3c7e83678b977e0e9771
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-02-20 15:47:22 +0000

    Check if logged in before refreshing user

--- appNekomp/src/androidMain/kotlin/com/chesire/nekomp/workers/RefreshUserWorker.kt
+++ appNekomp/src/androidMain/kotlin/com/chesire/nekomp/workers/RefreshUserWorker.kt
@@ -4,21 +4,22 @@ import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import co.touchlab.kermit.Logger
+import com.chesire.nekomp.library.datasource.auth.AuthRepository
 import com.chesire.nekomp.library.datasource.user.UserRepository
 import com.github.michaelbull.result.mapBoth
-import kotlinx.coroutines.flow.firstOrNull
 
 private const val MAX_RETRIES = 1
 
 class RefreshUserWorker(
     appContext: Context,
     workerParams: WorkerParameters,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val authRepository: AuthRepository
 ) : CoroutineWorker(appContext, workerParams) {
 
     override suspend fun doWork(): Result {
         Logger.d("RefreshUserWorker") { "Starting worker for refreshing user" }
-        return if (userRepository.user.firstOrNull() == null) {
+        return if (authRepository.accessToken().isNullOrBlank()) {
             Logger.d("RefreshUserWorker") { "No user stored, exiting early" }
             Result.success()
         } else {
