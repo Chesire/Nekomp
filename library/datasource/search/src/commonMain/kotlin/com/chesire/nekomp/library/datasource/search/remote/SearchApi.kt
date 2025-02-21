175f2c253e65acebd2a8b5424f4b54caf04e18c1
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-02-21 14:15:45 +0000

    Fix missing header for search call

--- library/datasource/search/src/commonMain/kotlin/com/chesire/nekomp/library/datasource/search/remote/SearchApi.kt
+++ library/datasource/search/src/commonMain/kotlin/com/chesire/nekomp/library/datasource/search/remote/SearchApi.kt
@@ -2,6 +2,7 @@ package com.chesire.nekomp.library.datasource.search.remote
 
 import com.chesire.nekomp.library.datasource.search.remote.model.SearchResponseDto
 import de.jensklingenberg.ktorfit.http.GET
+import de.jensklingenberg.ktorfit.http.Headers
 import de.jensklingenberg.ktorfit.http.Query
 
 private const val FIELDS = "synopsis,titles,canonicalTitle,subtype,posterImage"
@@ -9,11 +10,19 @@ private const val FIELDS = "synopsis,titles,canonicalTitle,subtype,posterImage"
 // TODO: These calls should support pagination, might be good to add at some point
 interface SearchApi {
 
+    @Headers(
+        "Accept: application/vnd.api+json",
+        "Content-Type: application/vnd.api+json"
+    )
     @GET("api/edge/anime?fields[anime]=$FIELDS")
     suspend fun searchForAnime(
         @Query("filter[text]") title: String
     ): Result<SearchResponseDto>
 
+    @Headers(
+        "Accept: application/vnd.api+json",
+        "Content-Type: application/vnd.api+json"
+    )
     @GET("api/edge/manga?fields[manga]=$FIELDS")
     suspend fun searchForManga(
         @Query("filter[text]") title: String
