c7175a99cd0813d30fccdbf0cff99c9a0bd1411b
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-02-27 11:42:50 +0000

    Begin updating the profile screen with user info

--- feature/profile/src/commonMain/kotlin/com/chesire/nekomp/feature/profile/ui/ProfileScreen.kt
+++ feature/profile/src/commonMain/kotlin/com/chesire/nekomp/feature/profile/ui/ProfileScreen.kt
@@ -1,18 +1,30 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 
 package com.chesire.nekomp.feature.profile.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.backhandler.BackHandler
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import com.chesire.nekomp.core.resources.NekoRes
 import nekomp.core.resources.generated.resources.nav_content_description_settings
 import org.jetbrains.compose.resources.stringResource
@@ -21,12 +33,14 @@ import org.koin.compose.viewmodel.koinViewModel
 @Composable
 fun ProfileScreen(
     viewModel: ProfileViewModel = koinViewModel(),
+    goBack: () -> Unit,
     navigateToSettings: () -> Unit
 ) {
     val state by viewModel.uiState.collectAsState()
 
     Render(
         state = state,
+        goBack = goBack,
         navigateToSettings = navigateToSettings,
         execute = { viewModel.execute(it) }
     )
@@ -35,14 +49,30 @@ fun ProfileScreen(
 @Composable
 private fun Render(
     state: UIState,
+    goBack: () -> Unit,
     navigateToSettings: () -> Unit,
     execute: (ViewAction) -> Unit
 ) {
+    // TODO: Switch to predictive
+    BackHandler { goBack() }
+
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(
+        rememberTopAppBarState()
+    )
     Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            TopAppBar(
+            LargeTopAppBar(
                 title = {
                     Text("Profile")
+                },
+                navigationIcon = {
+                    IconButton(onClick = goBack) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = null // Go back string
+                        )
+                    }
                 },
                 actions = {
                     IconButton(onClick = navigateToSettings) {
@@ -51,10 +81,93 @@ private fun Render(
                             contentDescription = stringResource(NekoRes.string.nav_content_description_settings)
                         )
                     }
-                }
+                },
+                scrollBehavior = scrollBehavior
             )
         }
-    ) {
-        Text("Profile")
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Text("Dummy data1")
+            Text("Dummy data23")
+            Text("Dummy data3")
+            Text("Dummy data4")
+            Text("Dummy data5")
+            Text("Dummy data6")
+            Text("Dummy data7")
+            Text("Dummy data8")
+            Text("Dummy data9")
+            Text("Dummy data0")
+            Text("Dummy data1")
+            Text("Dummy data2")
+            Text("Dummy data3")
+            Text("Dummy data4")
+            Text("Dummy data5")
+            Text("Dummy data6")
+            Text("Dummy data7")
+            Text("Dummy data8")
+            Text("Dummy data9")
+            Text("Dummy data0")
+            Text("Dummy data12")
+            Text("Dummy data2")
+            Text("Dummy data3")
+            Text("Dummy data4")
+            Text("Dummy data5")
+            Text("Dummy data6")
+            Text("Dummy data7")
+            Text("Dummy data8")
+            Text("Dummy data9")
+            Text("Dummy data0")
+            Text("Dummy data1")
+            Text("Dummy data2")
+            Text("Dummy data3")
+            Text("Dummy data4")
+            Text("Dummy data5")
+            Text("Dummy data6")
+            Text("Dummy data7")
+            Text("Dummy data8")
+            Text("Dummy data9")
+            Text("Dummy data1")
+            Text("Dummy data23")
+            Text("Dummy data5")
+            Text("Dummy data5")
+            Text("Dummy data6")
+            Text("Dummy data7")
+            Text("Dummy data8")
+            Text("Dummy data9")
+            Text("Dummy data09")
+            Text("Dummy data1")
+            Text("Dummy data2")
+            Text("Dummy data3")
+            Text("Dummy data4")
+            Text("Dummy data5")
+            Text("Dummy data6")
+            Text("Dummy data7")
+            Text("Dummy data8")
+            Text("Dummy data9")
+            Text("Dummy data1")
+            Text("Dummy data2")
+            Text("Dummy data3")
+            Text("Dummy data4")
+            Text("Dummy data5")
+            Text("Dummy data6")
+            Text("Dummy data7")
+            Text("Dummy data8")
+            Text("Dummy data9")
+            Text("Dummy data1")
+            Text("Dummy data2")
+            Text("Dummy data3")
+            Text("Dummy data4")
+            Text("Dummy data5")
+            Text("Dummy data6")
+            Text("Dummy data7")
+            Text("Dummy data8")
+            Text("Dummy data9")
+            Text("Dummy data1")
+        }
     }
 }
--- feature/profile/src/commonMain/kotlin/com/chesire/nekomp/feature/profile/ui/ProfileViewModel.kt
+++ feature/profile/src/commonMain/kotlin/com/chesire/nekomp/feature/profile/ui/ProfileViewModel.kt
@@ -1,15 +1,38 @@
 package com.chesire.nekomp.feature.profile.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.chesire.nekomp.core.model.Image
+import com.chesire.nekomp.core.preferences.ApplicationSettings
+import com.chesire.nekomp.core.preferences.ImageQuality
+import com.chesire.nekomp.library.datasource.user.UserRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class ProfileViewModel() : ViewModel() {
+class ProfileViewModel(
+    private val userRepository: UserRepository,
+    private val applicationSettings: ApplicationSettings
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow(UIState())
     val uiState: StateFlow<UIState> = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            userRepository.user.collect { user ->
+                val imageQuality = applicationSettings.imageQuality.first()
+                _uiState.update { state ->
+                    state.copy(coverImage = user.coverImage.toBestImage(imageQuality))
+                }
+            }
+        }
+    }
 
     fun execute(action: ViewAction) {
         when (action) {
@@ -20,6 +43,16 @@ class ProfileViewModel() : ViewModel() {
     private fun onObservedViewEvent() {
         _uiState.update { state ->
             state.copy(viewEvent = null)
+        }
+    }
+
+    private fun Image.toBestImage(imageQuality: ImageQuality): String {
+        return when (imageQuality) {
+            ImageQuality.Lowest -> lowest
+            ImageQuality.Low -> low
+            ImageQuality.Medium -> middle
+            ImageQuality.High -> high
+            ImageQuality.Highest -> highest
         }
     }
 }
