b4e33e410807f66f5a46acd2ae9b8c9cd31351b2
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-03-01 21:20:47 +0000

    Fix compilation issue with nonpublic class

--- library/datasource/library/src/commonMain/kotlin/com/chesire/nekomp/library/datasource/library/remote/model/EntryRequestDto.kt
+++ library/datasource/library/src/commonMain/kotlin/com/chesire/nekomp/library/datasource/library/remote/model/EntryRequestDto.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-internal data class EntryRequestDto(
+data class EntryRequestDto(
     @SerialName("data")
     val data: EntryDataRequestDto
 ) {
