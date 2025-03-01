8ea590f9654038294a79a6e7af51cfb00601a40c
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-03-01 08:52:55 +0000

    Move colors to own file

--- core/ui/src/commonMain/kotlin/com/chesire/nekomp/core/ui/NekompTheme.kt
+++ core/ui/src/commonMain/kotlin/com/chesire/nekomp/core/ui/NekompTheme.kt
@@ -2,43 +2,19 @@ package com.chesire.nekomp.core.ui
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Shapes
-import androidx.compose.material3.Typography
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.chesire.nekomp.core.ui.theme.darkTheme
+import com.chesire.nekomp.core.ui.theme.lightTheme
 
 @Composable
 fun NekompTheme(
     useDarkTheme: Boolean,
     content: @Composable () -> Unit
 ) {
-    val colors = if (useDarkTheme) {
-        darkColorScheme(
-            primary = Color(0xFFBB86FC),
-            secondary = Color(0xFF03DAC5),
-            tertiary = Color(0xFF3700B3)
-        )
-    } else {
-        lightColorScheme(
-            primary = Color(0xFF6200EE),
-            secondary = Color(0xFF03DAC5),
-            tertiary = Color(0xFF3700B3)
-        )
-    }
-    val typography = Typography(
-        bodyMedium = TextStyle(
-            fontFamily = FontFamily.Default,
-            fontWeight = FontWeight.Normal,
-            fontSize = 16.sp
-        )
-    )
+    val colors = if (useDarkTheme) darkTheme else lightTheme
     val shapes = Shapes(
         small = RoundedCornerShape(4.dp),
         medium = RoundedCornerShape(4.dp),
---
+++ core/ui/src/commonMain/kotlin/com/chesire/nekomp/core/ui/theme/Colors.kt
@@ -0,0 +1,17 @@
+package com.chesire.nekomp.core.ui.theme
+
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.graphics.Color
+
+val lightTheme = lightColorScheme(
+    primary = Color(0xFF6200EE),
+    secondary = Color(0xFF03DAC5),
+    tertiary = Color(0xFF3700B3)
+)
+
+val darkTheme = darkColorScheme(
+    primary = Color(0xFFBB86FC),
+    secondary = Color(0xFF03DAC5),
+    tertiary = Color(0xFF3700B3)
+)
