59ee989a7c36dc360b7b3fbe54366553afbb32ee
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-03-01 20:16:50 +0000

    Switch to koinInject

--- appNekomp/src/jvmMain/kotlin/com/chesire/nekomp/Main.kt
+++ appNekomp/src/jvmMain/kotlin/com/chesire/nekomp/Main.kt
@@ -4,12 +4,12 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.chesire.nekomp.library.datasource.trending.TrendingRepository
-import org.koin.compose.getKoin
+import org.koin.compose.koinInject
 
 fun main() {
     initKoin { }
     application {
-        val trending = getKoin().get<TrendingRepository>()
+        val trending = koinInject<TrendingRepository>()
         LaunchedEffect(Unit) {
             // Perform full sync of trending on start
             trending.performFullSync()
