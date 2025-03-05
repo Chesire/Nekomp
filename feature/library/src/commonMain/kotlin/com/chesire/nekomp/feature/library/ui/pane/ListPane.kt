d0c8dfb07a0e528d073c516ecbb580d9a9856bd4
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-03-05 15:25:29 +0000

    Put the status filters into the app bar

--- feature/library/src/commonMain/kotlin/com/chesire/nekomp/feature/library/ui/pane/ListPane.kt
+++ feature/library/src/commonMain/kotlin/com/chesire/nekomp/feature/library/ui/pane/ListPane.kt
@@ -1,4 +1,4 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
 
 package com.chesire.nekomp.feature.library.ui.pane
 
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -15,21 +16,27 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Sort
 import androidx.compose.material3.ElevatedFilterChip
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.VerticalDivider
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import com.chesire.nekomp.core.model.EntryStatus
 import com.chesire.nekomp.core.model.Type
 import com.chesire.nekomp.feature.library.data.ViewType
 import com.chesire.nekomp.feature.library.ui.Entry
 import com.chesire.nekomp.feature.library.ui.ViewAction
+import kotlin.math.absoluteValue
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.ImmutableMap
 import kotlinx.collections.immutable.persistentListOf
@@ -45,10 +52,52 @@ fun ListPane(
     execute: (ViewAction) -> Unit,
     onEntryClick: (Entry) -> Unit
 ) {
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(
+        rememberTopAppBarState()
+    )
     Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            TopAppBar(
-                title = { Text("Library") },
+            MediumTopAppBar(
+                title = {
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.Start),
+                        modifier = Modifier
+                            .horizontalScroll(rememberScrollState())
+                            .fillMaxWidth()
+                            .height(IntrinsicSize.Min)
+                            .padding(horizontal = 16.dp)
+                    ) {
+                        typeFilters.entries.forEach { typeFilter ->
+                            ElevatedFilterChip(
+                                selected = typeFilter.value,
+                                onClick = { execute(ViewAction.TypeFilterClick(typeFilter.key)) },
+                                label = { Text(text = typeFilter.key.name) }
+                            )
+                        }
+                        if (scrollBehavior.state.collapsedFraction < 0.65f) {
+                            Row(
+                                modifier = Modifier.alpha((scrollBehavior.state.collapsedFraction - 1f).absoluteValue),
+                                horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.Start)
+                            ) {
+                                VerticalDivider()
+                                statusFilters.entries.forEach { statusFilter ->
+                                    ElevatedFilterChip(
+                                        selected = statusFilter.value,
+                                        onClick = {
+                                            execute(
+                                                ViewAction.StatusFilterClick(
+                                                    statusFilter.key
+                                                )
+                                            )
+                                        },
+                                        label = { Text(text = statusFilter.key.name) }
+                                    )
+                                }
+                            }
+                        }
+                    }
+                },
                 actions = {
                     IconButton(onClick = { execute(ViewAction.ViewTypeClick) }) {
                         Icon(
@@ -62,36 +111,16 @@ fun ListPane(
                             contentDescription = "Sort"
                         )
                     }
-                }
+                },
+                scrollBehavior = scrollBehavior
             )
         }
     ) { paddingValues ->
-        Column(modifier = Modifier.padding(paddingValues)) {
-            // TODO: Hide on scroll down
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.Start),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(IntrinsicSize.Min)
-                    .padding(horizontal = 16.dp)
-                    .horizontalScroll(rememberScrollState())
-            ) {
-                typeFilters.entries.forEach { typeFilter ->
-                    ElevatedFilterChip(
-                        selected = typeFilter.value,
-                        onClick = { execute(ViewAction.TypeFilterClick(typeFilter.key)) },
-                        label = { Text(text = typeFilter.key.name) }
-                    )
-                }
-                VerticalDivider()
-                statusFilters.entries.forEach { statusFilter ->
-                    ElevatedFilterChip(
-                        selected = statusFilter.value,
-                        onClick = { execute(ViewAction.StatusFilterClick(statusFilter.key)) },
-                        label = { Text(text = statusFilter.key.name) }
-                    )
-                }
-            }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
             when (currentViewType) {
                 ViewType.List -> ListItemsPane(
                     entries = entries,
