8433267771d66e7035837f5b0743b96bce4cdfdd
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-03-18 20:44:22 +0000

    Add methods to get the mapping dao

--- core/database/src/commonMain/kotlin/com/chesire/nekomp/core/database/DatabaseModule.kt
+++ core/database/src/commonMain/kotlin/com/chesire/nekomp/core/database/DatabaseModule.kt
@@ -4,6 +4,7 @@ import androidx.room.RoomDatabase
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import com.chesire.nekomp.core.database.dao.AiringDao
 import com.chesire.nekomp.core.database.dao.LibraryEntryDao
+import com.chesire.nekomp.core.database.dao.MappingDao
 import com.chesire.nekomp.core.database.dao.TrendingDao
 import com.chesire.nekomp.core.database.dao.UserDao
 import kotlinx.coroutines.Dispatchers
@@ -22,6 +23,7 @@ val databaseModule = module {
     }
     single<AiringDao> { get<AppDatabase>().getAiringDao() }
     single<LibraryEntryDao> { get<AppDatabase>().getLibraryEntryDao() }
+    single<MappingDao> { get<AppDatabase>().getMappingDao() }
     single<TrendingDao> { get<AppDatabase>().getTrendingDao() }
     single<UserDao> { get<AppDatabase>().getUserDao() }
 }
