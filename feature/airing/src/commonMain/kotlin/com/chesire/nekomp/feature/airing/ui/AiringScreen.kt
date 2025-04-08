cd4e9e69302a64c30f1d78edd1ba39c1081bd80e
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-04-08 20:52:20 +0100

    Dependency updates

--- feature/airing/src/commonMain/kotlin/com/chesire/nekomp/feature/airing/ui/AiringScreen.kt
+++ feature/airing/src/commonMain/kotlin/com/chesire/nekomp/feature/airing/ui/AiringScreen.kt
@@ -1,4 +1,4 @@
-@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
+@file:OptIn(ExperimentalMaterial3Api::class)
 
 package com.chesire.nekomp.feature.airing.ui
 
@@ -17,7 +17,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -101,7 +100,7 @@ private fun Render(
                                     )
                                     Text(
                                         text = airingItem.airingTime.time.toString(),
-                                        style = MaterialTheme.typography.headlineSmallEmphasized,
+                                        style = MaterialTheme.typography.headlineSmall,
                                         color = Color.White,
                                         textAlign = TextAlign.Center,
                                         maxLines = 1,
