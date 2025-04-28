d1b0d8f7af384ae1d365e501d2950c0ec983e994
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-04-28 20:27:14 +0100

    Add kdoc explaining extension method

--- core/ext/src/commonMain/kotlin/com/chesire/nekomp/core/ext/StringExt.kt
+++ core/ext/src/commonMain/kotlin/com/chesire/nekomp/core/ext/StringExt.kt
@@ -1,5 +1,8 @@
 package com.chesire.nekomp.core.ext
 
+/**
+ * This replaces the usage of the deprecated capitalize method on String.
+ */
 fun String.capitalize(): String {
     return replaceFirstChar {
         if (it.isLowerCase()) it.titlecase() else it.toString()
