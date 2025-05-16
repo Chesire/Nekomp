294aa554e8b748f053cb3a8139b203c555266e63
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-05-16 20:03:32 +0100

    Add methods to pull each of the favorite types

--- library/datasource/favorite/src/commonMain/kotlin/com/chesire/nekomp/library/datasource/favorite/FavoriteType.kt
+++ library/datasource/favorite/src/commonMain/kotlin/com/chesire/nekomp/library/datasource/favorite/FavoriteType.kt
@@ -3,5 +3,16 @@ package com.chesire.nekomp.library.datasource.favorite
 enum class FavoriteType {
     Character,
     Anime,
-    Manga
+    Manga;
+
+    companion object {
+
+        fun fromString(input: String?): FavoriteType? {
+            return if (input == null) {
+                null
+            } else {
+                FavoriteType.entries.find { it.name.lowercase() == input.lowercase() }
+            }
+        }
+    }
 }
--- library/datasource/favorite/src/commonMain/kotlin/com/chesire/nekomp/library/datasource/favorite/local/FavoriteStorage.kt
+++ library/datasource/favorite/src/commonMain/kotlin/com/chesire/nekomp/library/datasource/favorite/local/FavoriteStorage.kt
@@ -1,8 +1,55 @@
 package com.chesire.nekomp.library.datasource.favorite.local
 
-class FavoriteStorage {
+import com.chesire.nekomp.core.database.dao.FavoriteDao
+import com.chesire.nekomp.core.database.entity.FavoriteEntity
+import com.chesire.nekomp.core.model.Image
+import com.chesire.nekomp.library.datasource.favorite.Favorite
+import com.chesire.nekomp.library.datasource.favorite.FavoriteType
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class FavoriteStorage(private val favoriteDao: FavoriteDao) {
+
+    val favoriteCharacters: Flow<List<Favorite>> = favoriteDao
+        .entries()
+        .map { entry ->
+            entry
+                .filter { FavoriteType.fromString(it.type) == FavoriteType.Character }
+                .map { it.toFavorite(FavoriteType.Character) }
+        }
+
+    val favoriteAnime: Flow<List<Favorite>> = favoriteDao
+        .entries()
+        .map { entry ->
+            entry
+                .filter { FavoriteType.fromString(it.type) == FavoriteType.Anime }
+                .map { it.toFavorite(FavoriteType.Anime) }
+        }
+
+    val favoriteManga: Flow<List<Favorite>> = favoriteDao
+        .entries()
+        .map { entry ->
+            entry
+                .filter { FavoriteType.fromString(it.type) == FavoriteType.Manga }
+                .map { it.toFavorite(FavoriteType.Manga) }
+        }
 
     suspend fun updateFavorites() {
 
+    }
+
+    private fun FavoriteEntity.toFavorite(type: FavoriteType): Favorite {
+        return Favorite(
+            type = type,
+            title = title,
+            image = Image(
+                tiny = posterImageTiny,
+                small = posterImageSmall,
+                medium = posterImageMedium,
+                large = posterImageLarge,
+                original = posterImageOriginal
+            ),
+            rank = rank
+        )
     }
 }
